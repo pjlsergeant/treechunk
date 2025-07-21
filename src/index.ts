569ba5f36279d4df7595c5930a19ede0e4e40f7b
Peter Sergeant <pete@clueball.com>
2025-07-21 17:29:56 +0400

    fixup readme add entrypoint

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { TreeChunker } from './chunk.js';
+export { parseMarkdown } from './markdownParser.js';
+export { BaseSummarizer } from './summarizer/base.js';
+
+export type { Chunk, ChunkCallback } from './chunk.js';
+export type { DocumentNode } from './markdownParser.js';
